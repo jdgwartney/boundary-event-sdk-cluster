4f9a80e67944b9aab974136038290338e200eff5
jdgwartney <davidg@boundary.com>
2014-06-18 13:52:43 -0700

    checkpoint on configuration of sdk machine

--- sdk_provision.sh
+++ sdk_provision.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/env bash
+
+echo "Installing required software"
+sudo apt-get install -y git sysv-rc-conf
+
+#echo "Install maven"
+#sudo apt-get install -y maven2
+
+#echo "Clone Boundary Event SDK"
+#git clone https://github.com/boundary/boundary-event-sdk.git
+#chown -R vagrant:vagrant boundary-event-sdk
+#cd boundard-event-sdk
+#cp src/test/scripts/daemon.sh /bin/plumgrid
+cp src/test/scripts/daemon-template.sh /etc/init.d/plumgrid
+
+gcc -o plumgrid daemon.c
+cp plumgrid /bin/plumgrid
+sysv-rc-conf plumgrid on
+
