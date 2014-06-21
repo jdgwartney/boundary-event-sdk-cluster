bce1ff8f0e7e8206f7e69d0c367c17365dd10964
jdgwartney <davidg@boundary.com>
2014-06-21 14:43:59 -0700

    Add configuration of the listening port in /etc

--- src/daemon.c
+++ src/daemon.c
@@ -1,16 +1,28 @@
-#include <stdio.h>
-#include <signal.h>
-#include <syslog.h>
+#include <ctype.h>
 #include <errno.h>
-#include <stdlib.h>
 #include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <libgen.h>
+#include <limits.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
-#define DAEMON_NAME "simpledaemon"
+struct Configuration
+{
+  int listeningPort;
+
+};
+
+typedef struct Configuration CONFIG; 
+
+
+CONFIG configuration;
 
     void daemonShutdown();
     void signal_handler(int sig);
@@ -147,7 +159,8 @@
         write(pidFilehandle, str, strlen(str));
     }
 
-    processLoop() {
+    void processLoop()
+    {
         int listenfd,connfd,n;
         struct sockaddr_in servaddr,cliaddr;
         socklen_t clilen;
@@ -159,10 +172,11 @@
         bzero(&servaddr,sizeof(servaddr));
         servaddr.sin_family = AF_INET;
         servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-        servaddr.sin_port=htons(listeningPort);
+        servaddr.sin_port=htons(configuration.listeningPort);
         bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
 
         listen(listenfd,1024);
+        syslog(LOG_INFO,"Listener started");
 
         for(;;)
         {
@@ -189,16 +203,146 @@
         }
     }
 
+    int getKeyValuePair(char *line,char *key,char *value)
+    {
+        char * lKey;
+        char * lValue;
+        lKey = strtok_r(line,":",&lValue);
+        strcpy(key,lKey);
+        strcpy(value,lValue);
+        return !(key && value);
+    }
+
+    int getParameter(char *line,CONFIG * config) 
+    {
+        char test[80], blah[80];
+        char *sep = "\\/:;=-";
+        char lline[MAX_CANON+1];
+        char key[MAX_CANON+1];
+        char value[MAX_CANON+1];
+        int result = 0;
+
+        strcpy(lline,line);
+
+        result = getKeyValuePair(lline,key,value);
+/*
+        fprintf(stdout,"key: %s\n",key);
+        fprintf(stdout,"value: %s\n",value);
+*/
+
+        /*
+         * TODO: Match the key to the configuration structure
+         * TODO: Ignoring result from getting key value pair
+         */
+         config->listeningPort = atoi(value);
+
+        return result;
+    }
+
+    int loadConfiguration(char * name,CONFIG * config)
+    {
+      FILE *fp = NULL;
+      char filePath[PATH_MAX+1];
+      struct stat fileStat;
+      sprintf(filePath,"/etc/%s",name);
+      char line[MAX_CANON+1];
+      int result = 0;
+
+
+      syslog(LOG_INFO,"Reading configuration file");
+
+      if(!lstat(filePath,&fileStat))
+      {
+/*          fprintf(stdout,"size = %lld\n",fileStat.st_size);*/
+      }
+
+      fp = fopen(filePath,"r");
+      if (fp)
+      {
+          do
+          {
+             fread(line,1024,1,fp);
+             getParameter(line,config);
+          } while(!feof(fp));
+      }
+      else 
+      {
+         fprintf (stderr, "%s: Couldn't open file %s; %s\n",name,filePath, strerror (errno));
+         result = errno;
+      }
+      if (fp)
+      {
+         fclose(fp);
+      }
+      return result;
+    }
+
+    int handleCommandLineArguments(int argc,char *argv[])
+    {
+       int aflag = 0;
+       int bflag = 0;
+       char * cvalue = NULL;
+       char c;
+       int index = 0;
+
+       opterr = 0;
+
+       while ((c = getopt (argc, argv, "abc:")) != -1)
+         switch (c)
+           {
+           case 'a':
+             aflag = 1;
+             break;
+           case 'b':
+             bflag = 1;
+             break;
+           case 'c':
+             cvalue = optarg;
+             break;
+           case '?':
+             if (optopt == 'c')
+               fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+             else if (isprint (optopt))
+               fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+             else
+               fprintf (stderr,
+                        "Unknown option character `\\x%x'.\n",
+                        optopt);
+             return 1;
+           default:
+             abort();
+           }
+/*       printf ("aflag = %d, bflag = %d, cvalue = %s\n",aflag, bflag, cvalue); */
+     
+       for (index = optind; index < argc; index++)
+       {
+           /*printf ("Non-option argument %s\n", argv[index]); */
+       }
+
+       return 0;
+    }
+    
+    void usage(char * programName)
+    {
+        fprintf(stderr,"usage: %s <port>\n",programName);
+        exit(0);
+    }
+
     int main(int argc, char * argv[])
     {
-        if (argc != 2)
-        {
-            /*fprintf(stderr,"argc: %d\n",argc);*/
-            fprintf(stderr,"usage: %s <port>\n",basename(argv[0]));
-            exit(0);
+        char * programName = basename(argv[0]);
+        int result = 0;
+
+	if (handleCommandLineArguments(argc,argv)) {
+            usage(programName);
         }
 
-        listeningPort = atoi(argv[1]);
+	result = loadConfiguration(programName,&configuration);
+	if (result)
+        {
+           exit(result);
+        }
+        fprintf(stdout,"port %d\n",configuration.listeningPort);
 
         /* Debug logging
         setlogmask(LOG_UPTO(LOG_DEBUG));
@@ -207,7 +351,7 @@
 
         /* Logging */
         setlogmask(LOG_UPTO(LOG_INFO));
-        openlog(DAEMON_NAME, LOG_CONS | LOG_PERROR, LOG_USER);
+        openlog(programName, LOG_CONS | LOG_PERROR, LOG_USER);
 
         syslog(LOG_INFO, "Daemon starting up");
 
@@ -220,9 +364,6 @@
         {
             syslog(LOG_INFO, "daemon says hello");
             processLoop();
-/*
-            sleep(1);
-*/
         }
-        return 0;
+        return result;
     }
